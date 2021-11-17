ea49c2a3407cf790181fce097f9522856b425a07
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-11-17 01:42:26 -0500

    improve next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+ const nextConfig = {
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx', 'mdx'],
 };
+
+module.exports = withPlugins(
+  [
+    withBundleAnalyzer,
+  ],
+  nextConfig,
+);

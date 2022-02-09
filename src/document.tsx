7c2a00e316a4265e7f26c05e809829722e9e527a
Yordis Prieto <yordis.prieto@gmail.com>
2022-02-08 18:20:46 -0800

    Update document.tsx

--- src/document.tsx
+++ src/document.tsx
@@ -3,11 +3,11 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export class WebsiteDocument extends Document {
   render() {
     return (
-      <Html lang="en" className="box-border antialiased scroll-smooth">
+      <Html lang="en" className="h-full box-border antialiased scroll-smooth">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         </Head>
-        <body className="text-lg sm:text-2xl font-sans font-normal tracking-normal bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-300">
+        <body className="w-full h-full text-lg sm:text-2xl font-sans font-normal tracking-normal bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-300">
           <Main />
           <NextScript />
         </body>

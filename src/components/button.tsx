8134973ad02b815a4c3d4aba12a371441d1b5706
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2022-04-30 14:54:34 -0400

    chore: fix eslint

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,6 +4,7 @@ export default function Button(props: PropsWithChildren<{}>) {
   return (
     <a
       target="_blank"
+      rel="noreferrer"
       href="https://bit.ly/3se7YYw"
       className="bg-purple-700 text-gray-200 rounded-lg border border-black-500 py-4 px-10 pointer font-normal shadow-md"
     >

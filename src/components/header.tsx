d1631661f301d014aeeb0e389a8aa962b4b51d09
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2022-04-30 18:48:03 -0400

    feat: add drawer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,8 @@
-import { Popover, Transition } from '@headlessui/react';
 import { ErlangOfficialLogo } from '@/components/erlang-official-logo';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import { RouterLink } from '@/components/router-link';
-import { Fragment } from 'react';
+import { useState } from 'react';
+import { Drawer } from '@/components/drawer';
 
 const navigation = [
   { name: 'Documentation', href: '/documentation' },
@@ -11,110 +11,92 @@ const navigation = [
   { name: 'Blog', href: '/blog' },
 ];
 
+function useHeader() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  function onDrawerClose() {
+    return setSidebarOpen(false);
+  }
+
+  function onOpenDrawer() {
+    return setSidebarOpen(true);
+  }
+
+  return {
+    sidebarOpen,
+    setSidebarOpen,
+    onDrawerClose,
+    onOpenDrawer,
+  };
+}
+
+function ErlangLogoHomeLink() {
+  return (
+    <RouterLink href="/">
+      <span className="sr-only">Erlang</span>
+      <ErlangOfficialLogo className="h-8 w-auto sm:h-10" />
+    </RouterLink>
+  );
+}
+
+function Menu(props: { onOpenDrawer: () => void }) {
+  return (
+    <div className="bg-gray-900 py-6">
+      <nav className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6" aria-label="Global">
+        <div className="flex items-center flex-1">
+          <div className="flex items-center justify-between w-full md:w-auto">
+            <ErlangLogoHomeLink />
+            <div className="-mr-2 flex items-center md:hidden">
+              <button
+                onClick={props.onOpenDrawer}
+                className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white"
+              >
+                <span className="sr-only">Open main menu</span>
+                <MenuIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+          <div className="hidden space-x-8 md:flex md:ml-10">
+            {navigation.map((item) => (
+              <RouterLink
+                key={item.name}
+                href={item.href}
+                className="text-base font-medium text-white hover:text-gray-300"
+              >
+                {item.name}
+              </RouterLink>
+            ))}
+          </div>
+        </div>
+        <div className="hidden md:flex md:items-center md:space-x-6">
+          <RouterLink
+            href="/get-started"
+            className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
+          >
+            Get Started
+          </RouterLink>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
 export function Header() {
+  const controller = useHeader();
   return (
     <div className="relative overflow-hidden">
-      <Popover as="header" className="relative">
-        <div className="bg-gray-900 py-6">
-          <nav
-            className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
-            aria-label="Global"
-          >
-            <div className="flex items-center flex-1">
-              <div className="flex items-center justify-between w-full md:w-auto">
-                <a href="/">
-                  <span className="sr-only">Erlang</span>
-                  <ErlangOfficialLogo className="h-8 w-auto sm:h-10" />
-                </a>
-                <div className="-mr-2 flex items-center md:hidden">
-                  <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
-                    <span className="sr-only">Open main menu</span>
-                    <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                  </Popover.Button>
-                </div>
-              </div>
-              <div className="hidden space-x-8 md:flex md:ml-10">
-                {navigation.map((item) => (
-                  <RouterLink
-                    key={item.name}
-                    href={item.href}
-                    className="text-base font-medium text-white hover:text-gray-300"
-                  >
-                    {item.name}
-                  </RouterLink>
-                ))}
-              </div>
-            </div>
-            <div className="hidden md:flex md:items-center md:space-x-6">
-              <RouterLink
-                href="/get-started"
-                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
-              >
-                Get Started
-              </RouterLink>
-            </div>
-          </nav>
-        </div>
+      <Menu onOpenDrawer={controller.onOpenDrawer} />
 
-        <Transition
-          as={Fragment}
-          enter="duration-150 ease-out"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="duration-100 ease-in"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <Popover.Panel focus className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
-            <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-              <div className="px-5 pt-4 flex items-center justify-between">
-                <div>
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    alt=""
-                  />
-                </div>
-                <div className="-mr-2">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
-                    <span className="sr-only">Close menu</span>
-                    <XIcon className="h-6 w-6" aria-hidden="true" />
-                  </Popover.Button>
-                </div>
-              </div>
-              <div className="pt-5 pb-6">
-                <div className="px-2 space-y-1">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-                <div className="mt-6 px-5">
-                  <a
-                    href="@/routes/components/hero#"
-                    className="block text-center w-full py-3 px-4 rounded-md shadow bg-indigo-600 text-white font-medium hover:bg-indigo-700"
-                  >
-                    Start free trial
-                  </a>
-                </div>
-                <div className="mt-6 px-5">
-                  <p className="text-center text-base font-medium text-gray-500">
-                    Existing customer?{' '}
-                    <a href="@/routes/components/hero#" className="text-gray-900 hover:underline">
-                      Login
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </Popover.Panel>
-        </Transition>
-      </Popover>
+      <Drawer anchor="right" open={controller.sidebarOpen} onDrawerClose={controller.onDrawerClose} title="Main Menu">
+        <div className="flex-1 flex-shrink-0 w-96 flex flex-col bg-white px-4 py-6">
+          <div className="flex justify-between items-center">
+            <ErlangLogoHomeLink />
+            <button className="hover:bg-blue-100 p-2 rounded-full" onClick={controller.onDrawerClose}>
+              <XIcon className="w-8 h-8 flex" />
+            </button>
+          </div>
+        </div>
+      </Drawer>
     </div>
   );
 }

5846d005a0bdbc7a244accc565afbc082b468fb2
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2022-04-30 20:21:38 -0400

    chore: improve features html markup

--- src/routes/components/features.tsx
+++ src/routes/components/features.tsx
@@ -2,105 +2,81 @@ import { SparklesIcon } from '@heroicons/react/outline';
 
 export function Features() {
   return (
-    <div className="bg-white py-32 overflow-hidden flex flex-col gap-24">
-      <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-        <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
-          <div>
-            <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-              <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
-            </span>
-          </div>
-          <div className="mt-6">
+    <div className="bg-white py-32 overflow-hidden flex flex-col gap-24 max-w-7xl mx-auto px-4">
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-2 lg:gap-24">
+        <div className="flex flex-col gap-6 lg:py-16">
+          <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
+            <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
+          </span>
+          <div className="flex flex-col gap-4">
             <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">Fault tolerant</h2>
-            <p className="mt-4 text-lg text-gray-500">
+            <p className="text-lg text-gray-500">
               The Erlang VM, known for running low-latency, distributed and fault-tolerant systems.
             </p>
           </div>
         </div>
-        <div className="mt-12 sm:mt-16 lg:mt-0">
-          <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-            <img
-              className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-              src="/placeholder.png"
-              alt="Pending"
-            />
-          </div>
-        </div>
+        <img
+          className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5"
+          src="/images/placeholder.svg"
+          alt="Pending"
+        />
       </div>
 
-      <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-        <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
-          <div>
-            <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-              <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
-            </span>
-          </div>
-          <div className="mt-6">
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-2 lg:gap-24">
+        <div className="flex flex-col gap-6 lg:py-16">
+          <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
+            <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
+          </span>
+          <div className="flex flex-col gap-4">
             <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">Concurrent</h2>
-            <p className="mt-4 text-lg text-gray-500">
+            <p className="text-lg text-gray-500">
               Erlang uses the actor model - shared-nothing concurrency via message
             </p>
           </div>
         </div>
-        <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
-          <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-            <img
-              className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-              src="/placeholder.png"
-              alt="Pending"
-            />
-          </div>
-        </div>
+        <img
+          className="lg:order-first w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5"
+          src="/images/placeholder.svg"
+          alt="Pending"
+        />
       </div>
 
-      <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-        <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
-          <div>
-            <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-              <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
-            </span>
-          </div>
-          <div className="mt-6">
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-2 lg:gap-24">
+        <div className="flex flex-col gap-6 sm:px-6 lg:py-16">
+          <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
+            <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
+          </span>
+          <div className="flex flex-col gap-4">
             <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">Functional</h2>
-            <p className="mt-4 text-lg text-gray-500">
+            <p className="text-lg text-gray-500">
               Multi-clause functions with pattern matching and guards are the building blocks of Erlang code.
             </p>
           </div>
         </div>
-        <div className="mt-12 sm:mt-16 lg:mt-0">
-          <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-            <img
-              className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-              src="/placeholder.png"
-              alt="Pending"
-            />
-          </div>
-        </div>
+        <img
+          className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5"
+          src="/images/placeholder.svg"
+          alt="Pending"
+        />
       </div>
 
-      <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-        <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
-          <div>
-            <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-              <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
-            </span>
-          </div>
-          <div className="mt-6">
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-2 lg:gap-24">
+        <div className="flex flex-col gap-6 lg:py-16">
+          <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
+            <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
+          </span>
+          <div className="flex flex-col gap-4">
             <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">Immutable</h2>
-            <p className="mt-4 text-lg text-gray-500">
+            <p className="text-lg text-gray-500">
               All data in Erlang is immutable, allowing for safer and easier-to-reason-about concurrency.
             </p>
           </div>
         </div>
-        <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
-          <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-            <img
-              className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-              src="/placeholder.png"
-              alt="Pending"
-            />
-          </div>
-        </div>
+        <img
+          className="lg:order-first w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5"
+          src="/images/placeholder.svg"
+          alt="Pending"
+        />
       </div>
     </div>
   );
